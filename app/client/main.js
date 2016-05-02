6082748b26b6615852657f2f5b52aee78e79f9dc
kevin <kevin@connectsb.nl>
2016-05-03 00:07:32 +0200

    created a local image file loader

--- app/client/main.js
+++ app/client/main.js
@@ -4,19 +4,42 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import './main.html';
 
 Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+    // counter starts at 0
+    this.counter = new ReactiveVar(0);
 });
 
 Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
+    counter() {
+        return Template.instance().counter.get();
+    },
 });
 
 Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+    'change #files'(event, instance) {
+        // increment the counter when button is clicked
+
+        //var startByte = event.target.getAttribute('data-startbyte');
+        //var endByte = event.target.getAttribute('data-endbyte');
+
+        var files = document.getElementById('files').files;
+        if (!files.length) {
+            alert('Please select a file!');
+            return;
+        }
+
+        var file = files[0];
+        //var start = parseInt(startByte) || 0;
+        //var stop = parseInt(endByte) || file.size - 1;
+
+        var reader = new FileReader();
+        reader.onload = function(evt) {
+            if (evt.target.readyState === FileReader.DONE) {
+                document.getElementById('image').src = evt.target.result;
+            }
+        };
+
+        //var blob = file.slice(start, stop + 1);
+        //reader.readAsBinaryString(blob);
+        reader.readAsDataURL(file);
+    },
 });

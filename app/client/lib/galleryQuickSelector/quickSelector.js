cbf16a4adfa841fb5a63e3966559a17ab95a33b7
kevin <kevin@connectsb.nl>
2016-09-19 21:55:02 +0200

    select picture by gaze

--- app/client/lib/galleryQuickSelector/quickSelector.js
+++ app/client/lib/galleryQuickSelector/quickSelector.js
@@ -11,5 +11,16 @@ Template.quickSelector.events({
 Template.quickSelector.helpers({
     galleryObjects: () => {
         return GalleryObjects.find({}, {limit: 3});
+    },
+    gallerySrc: (obj) => {
+        return 'src: url(' + obj.image + ')';
+    },
+    getPosition: (index) => {
+        const positions = {
+            0: '0 2 0',
+            1: '0 1 -5',
+            2: '0 1 5'
+        };
+        return positions[index];
     }
 });

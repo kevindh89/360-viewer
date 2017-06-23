5039e5eaa5407b3f87a4bf84b9f5c4dc17afb26a
kevindh89 <kevindh89@gmail.com>
2017-06-23 17:37:42 +0200

    Added mouseover cursor pointer effect

--- app/imports/lib/aframeComponents/mouseenterTracking.js
+++ app/imports/lib/aframeComponents/mouseenterTracking.js
@@ -9,10 +9,12 @@ AFRAME.registerComponent('mouseenter-tracking', {
         this.el.addEventListener('mouseenter', () => {
             $('.marker-model', $(this.el))[0].setAttribute('model-opacity', 1);
             $('.marker-label', $(this.el))[0].setAttribute('visible', 'true');
+            $('.a-canvas').addClass('vw-mouseover');
         });
         this.el.addEventListener('mouseleave', () => {
             $('.marker-model', $(this.el))[0].setAttribute('model-opacity', 0.5);
             $('.marker-label', $(this.el))[0].setAttribute('visible', 'false');
+            $('.a-canvas').removeClass('vw-mouseover');
         });
     }
 });
